f097b5ae769d86f43d362c2a295c12cd5bd393c4
viniciusyashuda <viniciusyashuda008@gmail.com>
2022-03-09 09:36:32 -0300

    Add project files.

---
+++ List1_Exercise4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace List1_Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number, i, div = 0;
+
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= number; i++)
+            {
+
+                if (number % i == 0)
+                {
+
+                    div += 1;
+
+                }
+
+            }
+            if (div == 2)
+            {
+
+                Console.WriteLine(number + " is a prime number");
+
+            }
+            else
+            {
+
+                Console.WriteLine(number + " is not a prime number");
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
